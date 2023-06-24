514f9ec4a7649714ea3311a81fa973726506edd1
Harlan Haller <harlanhaller06@gmail.com>
2023-06-23 22:37:43 -0400

    added motor extension code for arm extension

--- src/main/java/frc/robot/robots/Wheely.java
+++ src/main/java/frc/robot/robots/Wheely.java
@@ -1,30 +1,41 @@
 package frc.robot.robots;
 
 import java.util.Map;
+import java.util.Optional;
 
 import org.northernforce.gyros.NFRNavX;
 import org.northernforce.motors.NFRSparkMax;
+import org.northernforce.motors.NFRTalonFX;
+import org.northernforce.subsystems.arm.NFRArmMotorExtensionJoint;
+import org.northernforce.subsystems.arm.NFRArmMotorExtensionJoint.NFRArmMotorExtensionJointConfiguration;
 import org.northernforce.subsystems.drive.NFRDrive;
 import org.northernforce.subsystems.drive.NFRTankDrive;
 import org.northernforce.subsystems.drive.NFRTankDrive.NFRTankDriveConfiguration;
 import org.northernforce.util.NFRRobotContainer;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import static frc.robot.constants.WheelyConstants.*;
 
 public class Wheely implements NFRRobotContainer {
     private final NFRDrive drive;
+    private final NFRArmMotorExtensionJoint extensionJoint;
+    private final NFRTalonFX extensionMotor; //hack should be getable from joint class in future
+
     public Wheely()
     {
-        NFRTankDriveConfiguration config = new NFRTankDriveConfiguration(
+        NFRTankDriveConfiguration driveConfig = new NFRTankDriveConfiguration(
             "drive",
             22.3125,
             11.39,
@@ -45,17 +56,48 @@ public class Wheely implements NFRRobotContainer {
         NFRNavX navx = new NFRNavX();
         leftSide.setInverted(false);
         rightSide.setInverted(true);
-        drive = new NFRTankDrive(config, leftSide, rightSide, navx);
+        drive = new NFRTankDrive(driveConfig, leftSide, rightSide, navx);
+
+        NFRArmMotorExtensionJointConfiguration extensionConfig =  new NFRArmMotorExtensionJointConfiguration(
+            "extension",
+            new Transform3d(),
+            retractedArmLength,
+            extendedArmLength,
+            0,
+            metersPerRotation,
+            false,
+            0.05
+        );
+        TalonFXConfiguration extensionMotorConfig = new TalonFXConfiguration();
+        extensionMotorConfig.CurrentLimits.SupplyCurrentLimit = 40;
+        extensionMotorConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+        extensionMotorConfig.CurrentLimits.SupplyCurrentThreshold = 60;
+        extensionMotorConfig.CurrentLimits.SupplyTimeThreshold = 0.1;
+        extensionMotor = new NFRTalonFX(extensionMotorConfig, 11);
+        extensionJoint = new NFRArmMotorExtensionJoint(extensionConfig, extensionMotor, Optional.empty(), Optional.empty());
     }
     @Override
     public void bindOI(GenericHID driverHID, GenericHID manipulatorHID) {
         if (driverHID instanceof XboxController && manipulatorHID instanceof XboxController)
         {
             XboxController driverController = (XboxController)driverHID;
+            XboxController manipulatorController = (XboxController)manipulatorHID;
+
             drive.setDefaultCommand(drive.getDefaultDriveCommand(
                 () -> -MathUtil.applyDeadband(driverController.getLeftY(), 0.07),
                 () -> -MathUtil.applyDeadband(driverController.getRightX(), 0.07)
             ));
+
+            // will use when limit switches exist
+            // new JoystickButton(manipulatorController, XboxController.Button.kRightBumper.value)
+            //     .onTrue(extensionJoint.new ExtendUntilBoolean(forwardIsExtendCoefficient * armSpeed, () -> true)); //TODO make boolean supplier
+            // new JoystickButton(manipulatorController, XboxController.Button.kLeftBumper.value)
+            //     .onTrue(extensionJoint.new RetractUntilBoolean((-forwardIsExtendCoefficient) * armSpeed, () -> true)); //TODO make boolean supplier
+            
+            new JoystickButton(manipulatorController, XboxController.Button.kRightBumper.value)
+                .onTrue(extensionJoint.getExtendBySpeedCommand(armSpeed));
+            new JoystickButton(manipulatorController, XboxController.Button.kLeftBumper.value)
+                .onTrue(extensionJoint.getRetractBySpeedCommand(armSpeed));
         }
     }
     @Override
