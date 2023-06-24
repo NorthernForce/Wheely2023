212dcb90775bde3fa3b47bc23becbb0b05dfa434
Harlan Haller <harlanhaller06@gmail.com>
2023-06-23 22:58:11 -0400

    added arm retracted length constant

--- src/main/java/frc/robot/constants/WheelyConstants.java
+++ src/main/java/frc/robot/constants/WheelyConstants.java
@@ -2,8 +2,8 @@ package frc.robot.constants;
 
 public class WheelyConstants {
     public static final double metersPerRotation = 0.1; //10.13163 cm spool circumference
-    public static final double retractedArmLength = 0.2; //meters
-    public static final double extendedArmLength = 1; //meters
+    public static final double retractedArmLength = 1.06; //meters
+    public static final double extendedArmLength = 3; //meters
     public static final double armSpeed = 0.7;
     public static final double forwardIsExtendCoefficient = 1; //is it is 1 or -1 if it isn't
 }
